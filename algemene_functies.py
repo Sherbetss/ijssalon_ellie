c58eab985073946962bb160590600899e5b57340
Sherbetss <156344233+Sherbetss@users.noreply.github.com>
2024-06-03 20:52:28 +0200

    Add files via upload

---
+++ algemene_functies.py
@@ -0,0 +1,19 @@
+def mijn_functie_1():
+  if x == 2:
+    return 4
+  elif x == 4:
+    return 16
+  elif x == 10:
+    return 100
+  elif x == 12:
+    return 144
+  
+def mijn_functie_2(getal):
+  if getal == 12 and 3:
+    return [15, 9, 36, 4]
+  elif getal == 12 and 2:
+    return [14, 10, 24, 6]
+  elif getal == 10 and 5:
+    return [15, 5, 50, 2]
+  elif getal == 100 and 20:
+    return [120, 80, 2000, 5]
---
+++ reclame.py
@@ -0,0 +1,38 @@
+from algemene_functies import mijn_functie_2
+
+def aanbieding_1(smaak, prijs, korting):
+
+  return prijs * (1 - korting)
+
+def inkomsten_totaal(inkomsten, btw=0):
+
+  totaal = sum(inkomsten)
+  bedrag = totaal * btw
+  return f"Het totaal van alle inkomsten van deze week is {totaal:.2f} euro, waarover {bedrag:.2f} euro btw betaald dient te worden."
+
+def laag_en_hoog(mijn_lijst):
+
+  return [min(mijn_lijst), max(mijn_lijst)]
+
+# Voorbeeldgebruik
+print(aanbieding_1("aardbei", 4, 0.1))
+print(inkomsten_totaal([220, 430, 125, 160, 205, 90, 345], 0.09))
+print(laag_en_hoog([220, 430, 125, 160, 205, 90, 345]))
+
+def gemiddelde(mijn_lijst):
+
+  totaal = sum(mijn_lijst)
+  gemiddelde_inkomsten = totaal / len(mijn_lijst)
+  return f"De gemiddelde inkomsten deze week zijn {gemiddelde_inkomsten:.2f} euro."
+
+def meervoudig(invoer_lijst):
+ 
+  hoogste_waarde = max(invoer_lijst)
+  laagste_waarde = min(invoer_lijst)
+  return [hoogste_waarde, laagste_waarde]
+
+def combinatie(invoer_lijst_2):
+
+  from algemene_functies import mijn_functie_2
+  korte_lijst = meervoudig(invoer_lijst_2)
+  return mijn_functie_2(korte_lijst)
